682e6aa2117a807aa19a3a41038ae13068fc92f1
Adarsh Shukla <89727799+ada100820@users.noreply.github.com>
2023-09-04 19:32:34 +0000

    Variables in Javascript (const,let,var)

---
+++ 01-Basics/01.Variables.js
@@ -0,0 +1,29 @@
+const account_ID = 16312
+let account_Email = "adarsh@gitops"
+var account_pass = 123
+account_city = "Ludhiana"
+let account_state
+
+
+
+
+// account_ID=34 // not allowed
+
+account_Email="ada@chai"
+
+console.log(account_ID);
+
+
+/*
+
+Prefer not to use var because issue in block scope and functional scope
+
+*/
+
+console.table([account_Email, account_pass, account_city, account_state])
+
+
+
+
+
+
